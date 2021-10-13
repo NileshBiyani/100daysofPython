39328b62f49d61d2ca2dfb1a6fe9864a4e254bfa
NileshBiyani <56485056+NileshBiyani@users.noreply.github.com>
2021-10-14 01:02:55 +0530

    commit

--- 2nd day/Tip_calculator.py
+++ 2nd day/Tip_calculator.py
@@ -3,4 +3,4 @@ total_bill=int(input("What was the total bill? $"))
 total_people=int(input("How many people to split the bill? "))
 tip=int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 split_bill=((total_bill*tip*0.01)+total_bill)/total_people
-print("Each person should pay: "+"$"+str(split_bill))
+print(f"Each person should pay: ${round(split_bill,2)}")
